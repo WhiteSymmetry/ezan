f4df5e22f4c5cda1e6b9d3cda6d1871822997b78
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-02-20 12:49:50 +0300

    ezan

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 Ezan - Namaz vakitleri ve kıble hesaplama modülü.
 
+
+Kullanım: Usage
+---
+
+from ezan import print_prayer_times, get_user_location_and_date
+
+lat, lon, tz, date = get_user_location_and_date()
+print_prayer_times(lat, lon, tz, date)
+
--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
+# -*- coding: utf-8 -*-
 ## `setup.py`
 
 from setuptools import setup, find_packages
-import ezan.version
+import io
+import re
+import sys
+import os
+
+# UTF-8 encoding sorunlarını çöz
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def get_version():
+    with open('ezan/__init__.py', 'r', encoding='utf-8') as f:
+        content = f.read()
+    match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setup(
     name="ezan",
-    version=ezan.version.__version__,
+    version=get_version(),
     author="Mehmet Keçeci",
     author_email="mkececi@yaani.com",
     description="Namaz vakitleri ve kıble hesaplama aracı",
@@ -15,7 +33,6 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: AGPL-3.0-or-later License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.11",
